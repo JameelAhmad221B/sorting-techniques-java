6456195b03547a9483306a8f334a7ac517c60b86
Jameel Ahmad <114793613+JameelAhmad221B@users.noreply.github.com>
2022-12-31 22:56:51 +0500

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -16,7 +16,7 @@ public class SelectionSort {
     }
 	
     public static void selectionSort(int[] array) {
-			int minimum, temp;
+        int minimum, temp;
         for (int i = 0; i < array.length-1; i++) {
             minimum = i;
             for (int j = i+1; j < array.length; j++)
